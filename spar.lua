2a0d080fa81dc28136eb6bd4b685c48ec100b701
SpartanSf <swoshswosh@proton.me>
2025-05-25 11:04:56 -0500

    Update spar.lua

--- spar.lua
+++ spar.lua
@@ -1,6 +1,6 @@
 local Pine3D = require("Pine3D")
 
-local ThreeDFrame = Pine3D.newFrame()
+local ThreeDFrame = Pine3D.newFrame(_, _, _, _, true)
 ThreeDFrame:setCamera(-8, 0.6, 0)
 ThreeDFrame:setFoV(90)
 
@@ -29,6 +29,18 @@ function Vec3:length()
     return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
 end
 
+function Vec3:dot(v)
+    return self.x * v.x + self.y * v.y + self.z * v.z
+end
+
+function Vec3:cross(v)
+    return Vec3.new(
+        self.y * v.z - self.z * v.y,
+        self.z * v.x - self.x * v.z,
+        self.x * v.y - self.y * v.x
+    )
+end
+
 function Vec3:normalize()
     local len = self:length()
     if len == 0 then return Vec3.new(0, 0, 0) end
@@ -51,16 +63,31 @@ end
 PhysicsObject = {}
 PhysicsObject.__index = PhysicsObject
 
-function PhysicsObject.new(position, size, mass)
+function PhysicsObject.new(opts)
+    local position = opts.position
+    local size = opts.size
+    local mass = opts.mass
+    local compressed = opts.compressed or false
+    local model = opts.model
+
     local halfSize = size:scale(0.5)
-    table.insert(pineObjects, ThreeDFrame:newObject("models/pineapple", position.x, position.y, position.z))
+    if compressed then
+        table.insert(pineObjects, ThreeDFrame:newCompressedObject(model, position.x, position.y, position.z))
+    else
+        table.insert(pineObjects, ThreeDFrame:newObject(model, position.x, position.y, position.z))
+    end
     local obj = setmetatable({
         pineObject = #pineObjects,
         position = position,
         velocity = Vec3.new(0, 0, 0),
+        rotation = Vec3.new(0, 0, 0),
+        rotation_velocity = Vec3.new(0, 0, 0),
         force = Vec3.new(0, 0, 0),
         mass = mass or 1,
         size = size,
+        drag = opts.drag,
+        lifetime = opts.lifetime or nil,
+        age = 0,
         aabb = AABB.new(position:sub(halfSize), position:add(halfSize))
     }, PhysicsObject)
     return obj
@@ -71,11 +98,14 @@ function PhysicsObject:applyForce(force)
 end
 
 function PhysicsObject:update(dt)
+    self.age = self.age + dt
     local acceleration = self.force:scale(1 / self.mass)
 
     self.velocity = self.velocity:add(acceleration:scale(dt))
 
     self.position = self.position:add(self.velocity:scale(dt))
+
+    self.rotation = self.rotation:add(self.rotation_velocity:scale(dt))
 
     local halfSize = self.size:scale(0.5)
     self.aabb.min = self.position:sub(halfSize)
@@ -137,48 +167,153 @@ local function resolveCollision(obj1, obj2)
 
     local normal = mtv:normalize()
     local relVel = obj1.velocity:sub(obj2.velocity)
-    local velAlongNormal = relVel.x * normal.x + relVel.y * normal.y + relVel.z * normal.z
+    local velAlongNormal = relVel:dot(normal)
 
     if velAlongNormal > 0 then
         return
     end
 
     local restitution = 0.5
+    local friction = 0.3
+
     local j = -(1 + restitution) * velAlongNormal
     j = j / (1 / obj1.mass + 1 / obj2.mass)
-
     local impulse = normal:scale(j)
+
     obj1.velocity = obj1.velocity:add(impulse:scale(1 / obj1.mass))
     obj2.velocity = obj2.velocity:sub(impulse:scale(1 / obj2.mass))
+
+    local tangent = relVel:sub(normal:scale(velAlongNormal))
+    if tangent:length() > 0 then
+        tangent = tangent:normalize()
+        local jt = -relVel:dot(tangent)
+        jt = jt / (1 / obj1.mass + 1 / obj2.mass)
+
+        local frictionImpulse
+        if math.abs(jt) < j * friction then
+            frictionImpulse = tangent:scale(jt)
+        else
+            frictionImpulse = tangent:scale(-j * friction)
+        end
+
+        obj1.velocity = obj1.velocity:add(frictionImpulse:scale(1 / obj1.mass))
+        obj2.velocity = obj2.velocity:sub(frictionImpulse:scale(1 / obj2.mass))
+
+        local I1 = (1/6) * obj1.mass * obj1.size:length()^2
+        local I2 = (1/6) * obj2.mass * obj2.size:length()^2
+        local torque1 = frictionImpulse:cross(normal):scale(1 / I1)
+        local torque2 = frictionImpulse:cross(normal):scale(1 / I2)
+        obj1.rotation_velocity = obj1.rotation_velocity:add(torque1)
+        obj2.rotation_velocity = obj2.rotation_velocity:sub(torque2)
+    end
 end
 
+local objects = {}
 
+local function gameUpdate(dt)
+    local collisionedPairs = {}
 
-local obj1 = PhysicsObject.new(Vec3.new(0, 0, 0), Vec3.new(1, 1, 1), 2)
-local obj2 = PhysicsObject.new(Vec3.new(0, 5, 5), Vec3.new(1, 1, 1), 2)
+    for i = 1, #objects do
+        local obj = objects[i]
+        obj:applyGravity(-9.8, dt)
+        local v = obj.velocity
+        obj.velocity.x = v.x - obj.drag * v.x * math.abs(v.x) * dt
+        obj.velocity.y = v.y - obj.drag * v.y * math.abs(v.y) * dt
+        obj.velocity.z = v.z - obj.drag * v.z * math.abs(v.z) * dt
 
-obj1.velocity = Vec3.new(0, 0, 0)
-obj2.velocity = Vec3.new(0, -10, -10)
+        local dragFactor = 1 - obj.drag * dt
+        dragFactor = math.max(0, dragFactor)
+        obj.rotation_velocity.x = obj.rotation_velocity.x * dragFactor
+        obj.rotation_velocity.y = obj.rotation_velocity.y * dragFactor
+        obj.rotation_velocity.z = obj.rotation_velocity.z * dragFactor
 
-local dt = 0.016
+        for j = i + 1, #objects do
+            local obj2 = objects[j]
+            local key = tostring(obj) .. "-" .. tostring(obj2)
 
-
-while true do
-    if obj1.aabb:checkCollision(obj2.aabb) then
-        resolveCollision(obj1, obj2)
+            if not collisionedPairs[key] and obj.aabb:checkCollision(obj2.aabb) then
+                resolveCollision(obj, obj2)
+                collisionedPairs[key] = true
+            end
+        end
     end
 
-    obj1:applyGravity(-9.8, dt)
-    obj2:applyGravity(-9.8, dt)
-
-    obj1:update(dt)
-    obj2:update(dt)
-
-    pineObjects[obj1.pineObject]:setPos(obj1.position.x, obj1.position.y, obj1.position.z)
-    pineObjects[obj2.pineObject]:setPos(obj2.position.x, obj2.position.y, obj2.position.z)
+    local i = 1
+    while i <= #objects do
+        local obj = objects[i]
+        obj:update(dt)
+        if obj.lifetime and obj.age >= obj.lifetime then
+            pineObjects[obj.pineObject]:setModel({x1 = 0, x2 = 0, x3 = 0, y1 = 0, y2 = 0, y3 = 0, z1 = 0, z2 = 0, z3 = 0, c = 0})
+            table.remove(objects, i)
+        else
+            pineObjects[obj.pineObject]:setPos(obj.position.x, obj.position.y, obj.position.z)
+            pineObjects[obj.pineObject]:setRot(obj.rotation.x, obj.rotation.y, obj.rotation.z)
+            i = i + 1
+        end
+    end
 
     ThreeDFrame:drawObjects(pineObjects)
     ThreeDFrame:drawBuffer()
-
-    sleep(dt)
 end
+
+local obj1 = PhysicsObject.new({
+    model = "models/pineapple",
+    position = Vec3.new(0, 2, 0),
+    size = Vec3.new(1, 1, 1),
+    mass = 2,
+    drag = 0.1,
+    lifetime = 0.25
+})
+local obj2 = PhysicsObject.new({
+    model = "models/pineapple",
+    position = Vec3.new(0, 7, 5),
+    size = Vec3.new(1, 1, 1),
+    mass = 2,
+    drag = 0.1,
+    lifetime = 2
+})
+
+obj1.velocity = Vec3.new(0, 0, 0)
+obj2.velocity = Vec3.new(0, -10, -10)
+obj2.rotation_velocity = Vec3.new(0, 0, 10)
+
+table.insert(objects, obj1)
+table.insert(objects, obj2)
+
+local dt = 0.016
+
+local function gameLogic()
+    local start = os.epoch() / 100000
+    while true do
+        if (os.epoch() / 100000) - start > 0.1 then
+            start = os.epoch() / 100000
+            local newObj1 = PhysicsObject.new({
+                model = "models/pineapple",
+                position = Vec3.new(0, 2, 0),
+                size = Vec3.new(1, 1, 1),
+                mass = 2,
+                drag = 0.1,
+                lifetime = 2
+            })
+            local newObj2 = PhysicsObject.new({
+                model = "models/pineapple",
+                position = Vec3.new(0, 7, 5),
+                size = Vec3.new(1, 1, 1),
+                mass = 2,
+                drag = 0.1,
+                lifetime = 2
+            })
+
+            newObj1.velocity = Vec3.new(0, 0, 0)
+            newObj2.velocity = Vec3.new(0, -10, -10)
+            newObj2.rotation_velocity = Vec3.new(0, 0, 10)
+
+            table.insert(objects, newObj1)
+            table.insert(objects, newObj2)
+        end
+        gameUpdate(dt)
+        sleep(dt)
+    end
+end
+
+gameLogic()
